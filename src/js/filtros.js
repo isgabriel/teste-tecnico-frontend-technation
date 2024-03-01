b11de82b909c0ecfd8230c551e98be9bfe21667b
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-02-29 23:37:04 -0300

    feat: filtros por mes de cobranca, por mes de pagamento e por status das notas fiscais criados

--- src/js/filtros.js
+++ src/js/filtros.js
@@ -1,7 +1,7 @@
 import { criaCard } from "./criaCards.js";
 import { notasFiscais } from "./database.js";
 
-export const filtrarPorMes = (mes) => {
+export const filtrarPorMesDeEmissao = (mes) => {
     if (mes === "Selecionar mês") {
         criaCard(notasFiscais);
     } else {
@@ -9,6 +9,39 @@ export const filtrarPorMes = (mes) => {
             const dataEmissao = new Date(nota.emissao);
             return dataEmissao.getMonth() === mes - 1;
         });
+        criaCard(notasFiltradas);
+    }
+};
+export const filtrarPorMesDeCobranca = (mes) => {
+    if (mes === "Selecionar mês") {
+        criaCard(notasFiscais);
+    } else {
+        const notasFiltradas = notasFiscais.filter((nota) => {
+            const dataCobranca = new Date(nota.cobranca);
+            return dataCobranca.getMonth() === mes - 1;
+        });
+        criaCard(notasFiltradas);
+    }
+};
+export const filtrarPorMesDePagamento = (mes) => {
+    if (mes === "Selecionar mês") {
+        criaCard(notasFiscais);
+    } else {
+        const notasFiltradas = notasFiscais.filter((nota) => {
+            const dataPagamento = new Date(nota.pagamento);
+            return dataPagamento.getMonth() === mes - 1;
+        });
+        criaCard(notasFiltradas);
+    }
+};
+export const filtrarPorStatus = (status) => {
+    if (status === "Status da Nota") {
+        criaCard(notasFiscais);
+    } else {
+        const notasFiltradas = notasFiscais.filter(
+            (nota) => nota.status === status
+        );
+
         criaCard(notasFiltradas);
     }
 };
--- src/js/notasFiscais.js
+++ src/js/notasFiscais.js
@@ -1,6 +1,11 @@
 import { criaCard } from "./criaCards.js";
 import { notasFiscais } from "./database.js";
-import { filtrarPorMes } from "./filtros.js";
+import {
+    filtrarPorMesDeCobranca,
+    filtrarPorMesDeEmissao,
+    filtrarPorMesDePagamento,
+    filtrarPorStatus,
+} from "./filtros.js";
 
 export const mostrarNotasFiscais = () => {
     $("#dashboard").hide();
@@ -8,8 +13,20 @@ export const mostrarNotasFiscais = () => {
 
     criaCard(notasFiscais);
 
-    $("#filtro-por-mes").on("change", (e) => {
+    $("#filtro-por-mes-de-emissao").on("change", (e) => {
         const mesSelecionado = e.target.value;
-        filtrarPorMes(mesSelecionado);
+        filtrarPorMesDeEmissao(mesSelecionado);
+    });
+    $("#filtro-por-mes-de-cobranca").on("change", (e) => {
+        const mesSelecionado = e.target.value;
+        filtrarPorMesDeCobranca(mesSelecionado);
+    });
+    $("#filtro-por-mes-de-pagamento").on("change", (e) => {
+        const mesSelecionado = e.target.value;
+        filtrarPorMesDePagamento(mesSelecionado);
+    });
+    $("#filtro-por-status").on("change", (e) => {
+        const statusSelecionado = e.target.value;
+        filtrarPorStatus(statusSelecionado);
     });
 };
