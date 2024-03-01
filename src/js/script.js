0fd11524c07f6921085accd58c69dff5c5065fac
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-03-01 17:56:52 -0300

    feat: cria lógica de abrir/fechar menu mobile

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,13 @@ import {
     mostrarDashboard,
 } from "./dashboard.js";
 import { trocaFiltro } from "./filtros.js";
+import { trocaNav } from "./nav.js";
 import { mostrarNotasFiscais } from "./notasFiscais.js";
 
 $("#dashboard-link").on("click", mostrarDashboard);
 $("#notas-link").on("click", mostrarNotasFiscais);
 
+$("#btn-menu-mobile").on("click", trocaNav);
 $(".botao-filtro").on("click", trocaFiltro);
 
 $("select").on("click", () => {
